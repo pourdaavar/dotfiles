472b774b5f6a420097bbcb9d29920e1f0111b36e
Hossein Pourdavar <h.pourdavar@gmail.com>
2021-12-22 10:06:08 +0330

    :wrench: bookmarks added!

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,12 @@ set -g fish_greeting
 starship init fish | source
 
 source ~/.aliasrc
+source ~/.bookmarkrc
+
+function fish_user_key_bindings
+    bind \co gitmoji_fish
+end
+
+function open_d
+    open "$argv" </dev/null &>/dev/null &
+end
